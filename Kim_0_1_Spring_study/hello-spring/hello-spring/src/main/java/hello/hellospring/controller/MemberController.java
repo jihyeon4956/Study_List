235ff97fea84258d68ed9e6e4cd46e1a20353853
NJH <jihyeon4956@gmail.com>
2023-07-03 04:08:19 +0900

    회원가입, 회원조회 단순구현

--- Kim_0_1_Spring_study/hello-spring/hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
+++ Kim_0_1_Spring_study/hello-spring/hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,8 +1,15 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
 // MemberController는 MemberService를 통해서 회원가입을 하고 MemberController를 통해서 회원조회를 할 수 있어야 한다
 // => MemberController가 MemberController를 의존한다
 @Controller
@@ -12,5 +19,25 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping (value = "/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping(value = "/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+        return "redirect:/";
+    }
+    @GetMapping(value = "/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
---
+++ Kim_0_1_Spring_study/hello-spring/hello-spring/src/main/java/hello/hellospring/controller/MemberForm.java
@@ -0,0 +1,14 @@
+package hello.hellospring.controller;
+
+public class MemberForm { // 회원가입하는 폼 컨트롤러
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
+
