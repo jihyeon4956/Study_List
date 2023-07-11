117864b116aeb26a6b898402a795f65ecdd5d829
NJH <jihyeon4956@gmail.com>
2023-07-12 06:51:16 +0900

    임시2

--- Spring files/jpa-advance/src/main/java/com/sparta/jpaadvance/entity/User.java
+++ Spring files/jpa-advance/src/main/java/com/sparta/jpaadvance/entity/User.java
@@ -1,16 +1,30 @@
 package com.sparta.jpaadvance.entity;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
 @Setter
+//@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+
+    @ManyToMany(mappedBy = "userList")
+    private List<Food> foodList = new ArrayList<>();
+
+    public void addFoodList(Food food) {
+        this.foodList.add(food);
+        food.getUserList().add(this); // 외래 키(연관 관계) 설정
+    }
 }
