ea84cebc6136c30bc56351e2c2c5314f54dfae23
NJH <jihyeon4956@gmail.com>
2023-07-25 18:07:51 +0900

    Ex11_6

--- java_jungsuk3/src/main/java/com/jungsuk3/chap11/Ex11_6.java
+++ java_jungsuk3/src/main/java/com/jungsuk3/chap11/Ex11_6.java
@@ -51,23 +51,24 @@ class Student implements Comparable {
     }
 } // class Student
 
-class Exe11_6 {
+class Exercise11_6 {
     static int getGroupCount(TreeSet tset, int from, int to) {
-/*
-(1) . 알맞은 코드를 넣어 완성하시오
-*/
-
+        Student s1 = new Student("",0,0,from,from,from);
+        Student s2 = new Student("",0,0,to,to,to);
+        return tset.subSet(s1,s2).size();
     }
-}
-
     public static void main(String[] args) {
-        TreeSet set = new TreeSet(new Comparator() {
+        TreeSet set = new TreeSet(new Comparator() { // 익명클래스
             public int compare(Object o1, Object o2) {
-/*
-(2) . 알맞은 코드를 넣어 완성하시오
-*/
+                if(o1 instanceof Student && o2 instanceof Student) {
+                    Student s1 = (Student)o1;
+                    Student s2 = (Student)o2;
+                    return (int)(s1.getAverage() - s2.getAverage());
+                }
+                return -1;
             }
         });
+
         set.add(new Student("홍길동", 1, 1, 100, 100, 100));
         set.add(new Student("남궁성", 1, 2, 90, 70, 80));
         set.add(new Student("김자바", 1, 3, 80, 80, 90));
