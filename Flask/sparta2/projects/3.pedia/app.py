2dd1a67c6b754514155e0a7b5ea3b477d8f6a592
NJH <jihyeon4956@gmail.com>
2023-08-04 09:47:02 +0900

    Flask part2.week4_3(영화-별점추가)

--- Flask/sparta2/projects/3.pedia/app.py
+++ Flask/sparta2/projects/3.pedia/app.py
@@ -16,25 +16,24 @@ def home():
 def movie_post():
     url_receive = request.form['url_give']
     comment_receive = request.form['comment_give']
-
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-    data = requests.get(url_receive, headers=headers)
+    star_receive = request.form['star_give']
+    
+    headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+    data = requests.get(url_receive,headers=headers)
 
     soup = BeautifulSoup(data.text, 'html.parser')
 
-    ogimage = soup.select_one('meta[property="og:image"]')['content']
     ogtitle = soup.select_one('meta[property="og:title"]')['content']
     ogdesc = soup.select_one('meta[property="og:description"]')['content']
-
+    ogimage = soup.select_one('meta[property="og:image"]')['content']
 
     doc = {
         'title':ogtitle,
         'desc':ogdesc,
-		'image':ogimage,
-        'comment':comment_receive
+        'image':ogimage,
+        'comment':comment_receive,
+        'star':star_receive
     }
-
     db.movies.insert_one(doc)
 
     return jsonify({'msg':'저장완료!'})
