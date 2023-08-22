3600ef89576b5b505a62588c35e82da26bee7dc3
NJH <jihyeon4956@gmail.com>
2023-08-22 12:49:09 +0900

    [주특_1] 컨트롤러 CRUD 기본틀

--- Spring files/주특기주차/w1_spring-mvc/src/main/java/com/sparta/w1_springmvc/controller/HelloController.java
+++ Spring files/주특기주차/w1_spring-mvc/src/main/java/com/sparta/w1_springmvc/controller/HelloController.java
@@ -1,13 +1,36 @@
 package com.sparta.w1_springmvc.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
-@RestController
+@Controller
+@RequestMapping("/api")
 public class HelloController {
 
-    @GetMapping("/api/hello")
+    @GetMapping("/hello")
+    @ResponseBody // html이 아니라 순수한 문자열로 반환하는것
     public String hello() {
-        return "Hello World";
+        return "Hello World";  // 반환값을 templates에서 이 이름의 html이 있는지 찾아봄
+    }
+    @GetMapping("/get")
+    @ResponseBody
+    public String get() {
+        return "Get Method 요청";
+    }
+
+    @PostMapping("/post")
+    @ResponseBody
+    public String post() {
+        return "Post Method 요청";
+    }
+    @PutMapping("/put")
+    @ResponseBody
+    public String put() {
+        return "Put Method 요청";
+    }
+    @DeleteMapping("/delete")
+    @ResponseBody
+    public String delete() {
+        return "Delete Method 요청";
     }
 }
