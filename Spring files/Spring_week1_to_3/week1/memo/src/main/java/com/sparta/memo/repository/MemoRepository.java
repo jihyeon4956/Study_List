f29397f976a9b1425ac259dfc72909f4c11cdb45
NJH <jihyeon4956@gmail.com>
2023-08-24 06:51:28 +0900

    [주특_1] memo Ioc Container & Bean

--- Spring files/Spring_week1_to_3/week1/memo/src/main/java/com/sparta/memo/repository/MemoRepository.java
+++ Spring files/Spring_week1_to_3/week1/memo/src/main/java/com/sparta/memo/repository/MemoRepository.java
@@ -27,7 +27,7 @@ public class MemoRepository {
 
     public Memo save(Memo memo) {
         // DB 저장
-        KeyHolder keyHolder = new GeneratedKeyHolder(); // 기본 키를 반환받기 위한 객체
+        KeyHolder keyHolder = new GeneratedKeyHolder(); // 기본 키를 반환받기 위한 객체임
 
         String sql = "INSERT INTO memo (username, contents) VALUES (?, ?)";
         jdbcTemplate.update(con -> {
