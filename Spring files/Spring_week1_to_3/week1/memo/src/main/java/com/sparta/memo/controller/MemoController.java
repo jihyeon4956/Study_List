1fb9c15808283886e6439f018ced70270cf2328b
NJH <jihyeon4956@gmail.com>
2023-08-24 17:28:02 +0900

    [주특_1] memo update, delete 구현완료

--- Spring files/Spring_week1_to_3/week1/memo/src/main/java/com/sparta/memo/controller/MemoController.java
+++ Spring files/Spring_week1_to_3/week1/memo/src/main/java/com/sparta/memo/controller/MemoController.java
@@ -16,7 +16,7 @@ public class MemoController {
 
     private final Map<Long, Memo> memoList = new HashMap<>();
 
-
+    // 생성
     @PostMapping("/memos") // @RequestBody =-> 데이터는 Body 부분에 JSON 형태로 넘어올거라서 애노테이션 쓰기
     public MemoResponseDto createMemo(@RequestBody MemoRequestDto requestDto) {
         // RequestDto -> Entity로 저장하기 (데이터베이스와 소통하는 entity class로 변경해주기)
@@ -36,6 +36,7 @@ public class MemoController {
         return (new MemoResponseDto(memo));  // 중복제거
     }
 
+    // 조회
     @GetMapping("/memos")
     public List<MemoResponseDto> getMemos() {
         // Map To List
@@ -47,5 +48,39 @@ public class MemoController {
 //
         return memoList.values().stream()
                 .map(MemoResponseDto::new).toList();
+    }
+
+    // 수정
+    @PutMapping("/memos/{id}")  // 반환은 업데이트된 id값만 줄거임
+    public Long updateMemo(@PathVariable Long id, @RequestBody MemoRequestDto requestDto) {
+        // 데이터를 수정한다는것은 어떤 데이터에 대한 내용(contents와 username등)이 같이 넘어옴
+        // 클라이언트에서 Body부분에 넘어올거니까 @RequestBody로 받아줌, 넘어오는건 클라이언트에서 입력한 수정값임!!
+        // 넘어온 데이터를 받는건 RequestDto
+
+        // 해당 메모가 DB에 존재하는지 확인
+        if (memoList.containsKey(id)) {   // containsKey: Map의 메소드로  key에 받아온(id)값이 존재하는지 포함여부로 확인
+            // 해당 메모 가져오기
+            Memo memo = memoList.get(id);  // id에 해당하는 value를 (= Memo객체) 반환한다
+
+            // memo 수정
+            memo.update(requestDto);
+            return memo.getId();
+        } else {
+            throw new IllegalArgumentException("선택한 메모는 존재하지 않습니다.");
+        }
+    }
+
+    // 삭제
+    @DeleteMapping("/memos/{id}")
+    public Long deleteMemo(@PathVariable Long id) {
+        // 해당 메모가 DB에 존재하는지 확인
+        if (memoList.containsKey(id)) {
+            // 해당 메모 삭제하기
+            memoList.remove(id);   // Map 내장메서드임
+            return id;
+        } else {
+            throw new IllegalArgumentException("선택한 메모는 존재하지 않습니다.");
+        }
+
     }
 }
--- Spring files/Spring_week1_to_3/week1/memo/src/main/java/com/sparta/memo/entity/Memo.java
+++ Spring files/Spring_week1_to_3/week1/memo/src/main/java/com/sparta/memo/entity/Memo.java
@@ -13,7 +13,14 @@ public class Memo {
     private String username;
     private String contents;
 
+    // [생성자]
     public Memo(MemoRequestDto requestDto) {  // Controller에서 받아온 객체에 요소중 필요한것을 저장하여 Memo객체를 생성한다
+        this.username = requestDto.getUsername();
+        this.contents = requestDto.getContents();
+    }
+
+    // [메서드]
+    public void update(MemoRequestDto requestDto) { // qkedkdhs requestDto 로 수정해준다.
         this.username = requestDto.getUsername();
         this.contents = requestDto.getContents();
     }
