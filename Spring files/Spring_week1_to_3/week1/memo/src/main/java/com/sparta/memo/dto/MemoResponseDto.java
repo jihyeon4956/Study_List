0abcbdde202b04daea47a715ad51cf80cc62aded
NJH <jihyeon4956@gmail.com>
2023-08-24 18:15:47 +0900

    [주특_1] memo MySQL, Jdbc template 적용

--- Spring files/Spring_week1_to_3/week1/memo/src/main/java/com/sparta/memo/dto/MemoResponseDto.java
+++ Spring files/Spring_week1_to_3/week1/memo/src/main/java/com/sparta/memo/dto/MemoResponseDto.java
@@ -15,6 +15,12 @@ public class MemoResponseDto {
         this.contents = memo.getContents();
     }
 
+    public MemoResponseDto(Long id, String username, String contents) {
+        this.id = id;
+        this.username = username;
+        this.contents = contents;
+    }
+
     // ResponseDto가 Memo class와 같이 데이터베이스와 소통하는 클래스와 똑같은 형태가 대부분이다.
     // 같은 코드임에도 분리하는 이유는 데이터베이스와 소통하는 class는 조심스럽게 다뤄야하기 때문이다.
 }
