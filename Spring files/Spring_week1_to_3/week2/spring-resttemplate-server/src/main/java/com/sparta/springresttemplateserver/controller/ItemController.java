7e6f23ef1b1803819c4c1df9e2cdf5f01a565cda
NJH <jihyeon4956@gmail.com>
2023-08-27 12:40:07 +0900

    [주특_2-2] RestTemplate exchange server

---
+++ Spring files/Spring_week1_to_3/week2/spring-resttemplate-server/src/main/java/com/sparta/springresttemplateserver/controller/ItemController.java
@@ -0,0 +1,38 @@
+package com.sparta.springresttemplateserver.controller;
+
+import com.sparta.springresttemplateserver.dto.ItemResponseDto;
+import com.sparta.springresttemplateserver.dto.UserRequestDto;
+import com.sparta.springresttemplateserver.entity.Item;
+import com.sparta.springresttemplateserver.service.ItemService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/server")
+public class ItemController { // 포스트맨의 요청을 받는게 아니라 클라이언트 입장의 서버에서 보내온 RestTemplate 요청을 받는 컨트롤러다
+
+    private final ItemService itemService;
+
+    public ItemController(ItemService itemService) {
+        this.itemService = itemService;
+    }
+
+    @GetMapping("/get-call-obj")
+    public Item getCallObject(@RequestParam String query) { // @RequestParam: client에서 ?로 넘어오게 만들었음
+        return itemService.getCallObject(query);
+    }
+
+    @GetMapping("/get-call-list")
+    public ItemResponseDto getCallList() {
+        return itemService.getCallList();
+    }
+
+    @PostMapping("/post-call/{query}")
+    public Item postCall(@PathVariable String query, @RequestBody UserRequestDto requestDto) {
+        return itemService.postCall(query, requestDto);
+    }
+
+    @PostMapping("/exchange-call")
+    public ItemResponseDto exchangeCall(@RequestHeader("X-Authorization") String token, @RequestBody UserRequestDto requestDto) {
+        return itemService.exchangeCall(token, requestDto);
+    }
+}
