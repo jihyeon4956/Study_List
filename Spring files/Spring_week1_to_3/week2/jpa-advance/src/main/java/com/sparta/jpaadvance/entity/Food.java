9cfc6daa97ba797436557501d457ed8418591ba9
NJH <jihyeon4956@gmail.com>
2023-08-29 14:55:47 +0900

    [주특_2-2] 영속성 전의 JPA, FetchType 중복설정

--- Spring files/Spring_week1_to_3/week2/jpa-advance/src/main/java/com/sparta/jpaadvance/entity/Food.java
+++ Spring files/Spring_week1_to_3/week2/jpa-advance/src/main/java/com/sparta/jpaadvance/entity/Food.java
@@ -4,12 +4,8 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Entity
-@Getter
-@Setter
+@Getter @Setter
 @Table(name = "food")
 public class Food {
     @Id
--- Spring files/Spring_week1_to_3/week2/jpa-advance/src/main/java/com/sparta/jpaadvance/entity/User.java
+++ Spring files/Spring_week1_to_3/week2/jpa-advance/src/main/java/com/sparta/jpaadvance/entity/User.java
@@ -17,6 +17,15 @@ public class User {
     private Long id;
     private String name;
 
-    @OneToMany(mappedBy = "user")
+    // User Entity만 Save하면 foodList에 들어있는 Food Entity도 저장되게 만들고싶음 -> 영속성 전이에 persist
+    // 영속성 전의를 사용해서 저장하려고 하는 연관된 Entity의 애노테이션에  cascade = CascadeType.PERSIST를 해주면 된다
+//    @OneToMany(mappedBy = "user", cascade = CascadeType.PERSIST)  // 영속성 전이 설정함
+//    @OneToMany(mappedBy = "user")
+    @OneToMany(mappedBy = "user", cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
     List<Food> foodList = new ArrayList<>();
+
+    public void addFoodList(Food food) {
+        this.foodList.add(food);
+        food.setUser(this);
+    }
 }
