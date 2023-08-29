3a31da823259f5bf262ba98c59bd0808d5e70137
NJH <jihyeon4956@gmail.com>
2023-08-29 23:36:51 +0900

    [주특_2-2] 중간테이블 Order에 주문일 추가_검증

--- Spring files/Spring_week1_to_3/week2/jpa-advance/src/test/java/com/sparta/jpaadvance/JpaAdvanceApplicationTests.java
+++ Spring files/Spring_week1_to_3/week2/jpa-advance/src/test/java/com/sparta/jpaadvance/JpaAdvanceApplicationTests.java
@@ -2,8 +2,10 @@ package com.sparta.jpaadvance;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootTest
+@EnableJpaAuditing
 class JpaAdvanceApplicationTests {
 
     @Test
