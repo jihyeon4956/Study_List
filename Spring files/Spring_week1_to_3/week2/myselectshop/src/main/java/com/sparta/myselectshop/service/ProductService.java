c6361ebf8ff64b92443b69fe3a1cedb0ce410759
NJH <jihyeon4956@gmail.com>
2023-08-30 20:42:08 +0900

    [주특_2-2] Shop 관심상품 희망최거자 Service 구현완료

--- Spring files/Spring_week1_to_3/week2/myselectshop/src/main/java/com/sparta/myselectshop/service/ProductService.java
+++ Spring files/Spring_week1_to_3/week2/myselectshop/src/main/java/com/sparta/myselectshop/service/ProductService.java
@@ -1,21 +1,40 @@
 package com.sparta.myselectshop.service;
 
+import com.sparta.myselectshop.dto.ProductMypriceRequestDto;
 import com.sparta.myselectshop.dto.ProductRequestDto;
 import com.sparta.myselectshop.dto.ProductResponseDto;
 import com.sparta.myselectshop.entity.Product;
 import com.sparta.myselectshop.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class ProductService {
 
     private final ProductRepository productRepository;
+    private static final int MIN_MY_PRICE = 100;
 
     public ProductResponseDto createProduct(ProductRequestDto requestDto) {
         // 받아온 Dto를 우리가 저장할 Entity형태로 변환해줘야 함
         Product product = productRepository.save(new Product(requestDto));
+        return new ProductResponseDto(product);
+    }
+
+    @Transactional  // 변경감지가 필요한곳에는 꼭 사용하기
+    public ProductResponseDto updateProduct(Long id, ProductMypriceRequestDto requestDto) {
+        // 1. 가격 설정하기
+        int requestedMyprice = requestDto.getMyprice(); // 단, 최저가는 100원 이상이어야 한다
+        if (requestedMyprice < MIN_MY_PRICE) {
+            throw new IllegalArgumentException("유효하지 않은 가격입니다. 최소 " + MIN_MY_PRICE + "원 이상으로 설정해 주세요.");
+        }
+        // 2.받아온 id에 해당하는 데이터가 있는지 확인
+        Product product = productRepository.findById(id).orElseThrow(() ->
+                new NullPointerException("해당 상품을 찾을 수 없습니다."));
+
+        product.update(requestDto);
+
         return new ProductResponseDto(product);
     }
 }
