a666913c4f1a1fc3f55b5fa7cb53b18a4d7c9567
JH <jihyeon4956@gmail.com>
2023-09-19 07:53:01 +0900

    Nested, Order Test

--- Spring files/Spring_week1_to_3/junit5-prac/src/test/java/com/sparta/junit5prac/CustomTest.java
+++ Spring files/Spring_week1_to_3/junit5-prac/src/test/java/com/sparta/junit5prac/CustomTest.java
@@ -1,8 +1,6 @@
 package com.sparta.junit5prac;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 public class CustomTest {
     @Test
@@ -10,22 +8,33 @@ public class CustomTest {
     void test1() {
         System.out.println("테스트의 수행 내용들을 빠르게 파악할 수 있습니다.");
     }
-
     @Nested
     @DisplayName("주제 별로 테스트를 그룹지어서 파악하기 좋습니다.")
+    @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
     class Test1 {
+
+        @Order(1)
+        @Test
+        @DisplayName("Test1 클래스")
+        void test() {
+            System.out.println("\nTest1 클래스");
+        }
+
+        @Order(3)
         @Test
         @DisplayName("Test1 - test1()")
         void test1() {
             System.out.println("Test1.test1");
         }
 
+        @Order(2)
         @Test
         @DisplayName("Test1 - test2()")
         void test2() {
             System.out.println("Test1.test2");
         }
     }
+
 
     @Nested
     @DisplayName("Test2 다른 주제")
